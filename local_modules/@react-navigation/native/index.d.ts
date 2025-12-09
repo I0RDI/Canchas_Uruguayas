823a96c4f0bd153b3df883fd5032c259d37440af
I0RDI <jorge.andres1999@outlook.com>
2025-12-08 21:01:34 -0600

    Fix Caja focus hook and center close-day button

--- local_modules/@react-navigation/native/index.d.ts
+++ local_modules/@react-navigation/native/index.d.ts
@@ -1,3 +1,4 @@
 import * as React from 'react';
 
 export declare const NavigationContainer: React.ComponentType<React.PropsWithChildren<unknown>>;
+export declare function useFocusEffect(effect: React.EffectCallback): void;
